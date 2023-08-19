85fad338e38e0ea8200dd29a26f318f60048d577
wdy <mygit@wdy.com>
2023-08-19 16:17:51 +0800

    refactor: 代码提交

---
+++ backend_admin/src/main/java/com/wx/backend_admin/BackendAdminApplication.java
@@ -0,0 +1,14 @@
+package com.wx.backend_admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendAdminApplication.class, args);
+        System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>========wx购物管理系统启动成功========>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+    }
+
+}
